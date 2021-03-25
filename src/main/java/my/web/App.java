26dbb6aa1bf8bc1eda4a749eeaf19f37c0719295
Vitalii Salykin <salykin@yahoo.com>
2021-03-25 17:26:06 +0200

    print all environment

--- src/main/java/my/web/App.java
+++ src/main/java/my/web/App.java
@@ -1,10 +1,22 @@
 package my.web;
 
 
-public class App 
+import java.util.Map;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "My started!1" );
+    public static void main(String[] args) {
+        int PORT;
+        System.out.println("[MY OUT] ================================================= My started!2");
+        PORT = Integer.parseInt(System.getenv("PORT"));
+        System.out.println("System.getenv(\"PORT\"):" + PORT);
+        System.out.println("=======================================================================");
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n", envName, env.get(envName));
+        }
+        System.out.println("=======================================================================");
     }
+
+
 }
