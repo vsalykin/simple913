74e3c937c878e08f41aaba7a008a64bde0e9bed1
Vitalii Salykin <salykin@yahoo.com>
2021-03-28 04:00:19 +0300

    http-server 01 BAD: jetty dependency

--- src/main/java/my/web/App.java
+++ src/main/java/my/web/App.java
@@ -1,12 +1,13 @@
 package my.web;
 
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class App
+//import org.eclipse.jetty.server.Server;
+
+public class App extends HttpServlet
 {
     public static void main(String[] args) {
         int PORT;
@@ -15,53 +16,26 @@ public class App
         PORT = Integer.parseInt(System.getenv("PORT"));
         My.outPrintln("System.getenv(\"PORT\"):" + PORT);
         try{
-            var server = new Server(PORT);
-            server.Go();
+
+
+//            Server server = new Server(PORT);
+//            Server server =
         }
         catch(Exception e){My.errPrintln("MyException: " + e.toString());}
         catch(Error e){My.errPrintln("MyError: " + e.toString());}
         My.outPrintln("================================================= " + "[END]");
     }
 
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        My.outPrintln("request=" + request);
+        My.outPrintln("response=" + response);
+        response.getWriter().print("Хаюшки!");
+    }
 }
 
-class Server
+class My
 {
-    private final ServerSocket serversocket;
-    private final Socket socket;
-    private final InputStream inputstream;
-    private final OutputStream outputstream;
-    private final Scanner scanner;
-    private final PrintWriter printwriter;
-
-    public Server(int port) throws IOException {
-        serversocket = new ServerSocket(port);
-        My.outPrintln("Server wait for connection...");
-        socket = serversocket.accept(); //wait for request
-        My.outPrintln("... connection!");
-        inputstream = socket.getInputStream();
-        outputstream = socket.getOutputStream();
-        scanner = new Scanner(inputstream, StandardCharsets.UTF_8);
-        printwriter = new PrintWriter(new OutputStreamWriter(outputstream, StandardCharsets.UTF_8),true);
-        My.outPrintln("Server created.");
-    }
-
-    public void Go(){
-        printwriter.println("Hello!\n" +
-                "Please connect to me on https://simple913.herokuapp.com/:" + socket.getPort() + "\n" +
-                "Enter BYE to exit.");
-        while(scanner.hasNextLine()){
-            String s = scanner.nextLine();
-            My.outPrintln("Server got: " + s);
-            if(s.trim().equals("BYE")) break;
-            printwriter.println("Нравится! (" + s + ")");
-        }
-        My.outPrintln("Connection closed.");
-    }
-}
-
-class My{
     public static String out(String string){
         return "[MY OUT] " + string;
     }
