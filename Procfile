ff53c2a8a0f479baa36f9984f7b7ef7e4b72eb68
Vitalii Salykin <salykin@yahoo.com>
2021-03-28 05:19:47 +0300

    http-server 02 Hello (-1)

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:    java -cp target/classes:target/dependency/* my.web.App
+beb:    java -cp target/classes:target/dependency/* my.web.App
--- src/main/java/my/web/App.java
+++ src/main/java/my/web/App.java
@@ -1,46 +1,26 @@
 package my.web;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-
-
-public class App extends HttpServlet
+public class App
 {
     public static void main(String[] args) {
         int PORT;
 
-        My.outPrintln("================================================= " + "My started!003");
+        My.outPrintln("================================================= " + "My started!004 Jetty");
         PORT = Integer.parseInt(System.getenv("PORT"));
         My.outPrintln("System.getenv(\"PORT\"):" + PORT);
         try{
-            var server = new Server(PORT);
-            var context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-            context.setContextPath("/");
-            server.setHandler(context);
-            context.addServlet(new ServletHolder(new App()),"/*");
-            server.start();
-            server.join();
+//            var server = new Server(PORT);
+//            server.Go();
         }
         catch(Exception e){My.errPrintln("MyException: " + e.toString());}
         catch(Error e){My.errPrintln("MyError: " + e.toString());}
         My.outPrintln("================================================= " + "[END]");
     }
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        My.outPrintln("request=" + request);
-        My.outPrintln("response=" + response);
-        response.getWriter().print("Хаюшки!");
-    }
+
 }
 
-class My
-{
+class My{
     public static String out(String string){
         return "[MY OUT] " + string;
     }
@@ -53,7 +33,6 @@ class My
     public static void errPrintln(String string){
         System.out.println(err(string));
     }
-
 
     private My(){};
 }
